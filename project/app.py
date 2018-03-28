410512b807b4e6dd96e39d1ae62b70934247215b
KseniiaL <ksuimnetz@gmail.com>
2018-03-28 19:03:07 +0300

    [Add] Added initial page parsing

--- project/app.py
+++ project/app.py
@@ -1,6 +1,9 @@
 import requests
 from urllib.parse import urlparse
 import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+import functools
+import re
 
 links_file_path = './links.xml'
 html_files_path = './documents/'
@@ -25,6 +28,12 @@ def indent(elem, level=0):
     if level and (not elem.tail or not elem.tail.strip()):
       elem.tail = i
 
+def compose(*functions):
+    return functools.reduce(lambda f, g: lambda x: f(g(x)), functions, lambda x: x)
+
+def trim(string):
+    return string.strip()
+
 def save_response(link, res):
     name = urlparse(link)
     open(html_files_path + '{}.html'.format(name[1] + name[2].replace('/', '-')), 'w').write(res)
@@ -39,12 +48,34 @@ def get_html(link):
         # save_response(link, str(err))
         return str(err)
 
+def parse(html):
+    results = []
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    def validateEmail(email):
+        if len(email) > 7:
+            if re.match("^.+@([?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$)", email) != None:
+                return email
+
+    def deep(parent):
+        for el in parent.find_all('a'):
+            if el:
+                if (el.string != None):
+                    if (validateEmail(el.string) != None):
+                        results.append(el.string)
+
+    deep(soup)
+    for i in results:
+        ET.SubElement(res, 'email').text = str(i)
+    return results
+
 tree = ET.parse(links_file_path).getroot()
 for link in tree.findall('url'):
         res = ET.SubElement(root, 'result')
         ET.SubElement(res, 'url').text = link.text
         # print (link.text)
-        get_html(link.text)
+        compose(parse, get_html, trim)(link.text)
 
 indent(root)
 resTree = ET.ElementTree(root)
