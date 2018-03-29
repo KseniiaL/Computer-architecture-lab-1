1a37d27108f525859c2d00afbb033e8d740ce5a3
KseniiaL <ksuimnetz@gmail.com>
2018-03-29 21:49:16 +0300

    [Add] Pep8 standart

--- project/app.py
+++ project/app.py
@@ -1,6 +1,6 @@
 import requests
 from urllib.parse import urlparse
-import xml.etree.ElementTree as ET
+import xml.etree.ElementTree as elTree
 from bs4 import BeautifulSoup
 import functools
 import re
@@ -13,110 +13,114 @@ headers = {
 }
 visitedList = []
 includingLevel = 5
-root = ET.Element('data')
+root = elTree.Element('data')
+
 
 def indent(elem, level=0):
-  i = "\n" + level*"  "
-  if len(elem):
-    if not elem.text or not elem.text.strip():
-      elem.text = i + "  "
-    if not elem.tail or not elem.tail.strip():
-      elem.tail = i
-    for elem in elem:
-      indent(elem, level+1)
-    if not elem.tail or not elem.tail.strip():
-      elem.tail = i
-  else:
-    if level and (not elem.tail or not elem.tail.strip()):
-      elem.tail = i
+    i = "\n" + level * "  "
+    if len(elem):
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+        for elem in elem:
+            indent(elem, level + 1)
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+
 
 def compose(*functions):
     return functools.reduce(lambda f, g: lambda x: f(g(x)), functions, lambda x: x)
 
+
 def trim(string):
     return string.strip()
 
-def save_response(link, res):
-    name = urlparse(link)
-    open(html_files_path + '{}.html'.format(name[1] + name[2].replace('/', '-')), 'w').write(res)
 
-def get_html(link):
+def save_response(file_link, result):
+    name = urlparse(file_link)
+    open(html_files_path + '{}.html'.format(name[1] + name[2].replace('/', '-')), 'w').write(result)
+
+
+def get_html(file_link):
     try:
-        response = requests.get(link, headers=headers)
+        response = requests.get(file_link, headers=headers)
         html = str(response.text)
-        # save_response(link, html)
+        # save_response(file_link, html)
         return html
     except requests.exceptions.RequestException as err:
-        # save_response(link, str(err))
+        # save_response(file_link, str(err))
         return str(err)
+
 
 def parse(html):
     results = []
-    urlsList=[]
+    urls_list = []
 
     soup = BeautifulSoup(html, 'html.parser')
 
-    def validateEmail(email):
+    def validate_email(email):
         if len(email) > 7:
-            if ((re.match("^.+@([?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$)", email) != None) or
-                (re.match("[a-zA-Z0-9-.].+\(at\)([?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$)", email) != None)):
+            if re.match("^.+@([?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$)", email) or re.match("[a-zA-Z0-9-.].+\(at\)([?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$)", email):
                 return email
 
-    def validateUrl(url):
+    def validate_url(url):
         if re.match('https?://(?:www)?(?:[\w-]{2,255}(?:\.\w{2,6}){1,2})(?:/[\w&%?#-]{1,300})?', url):
             return url
 
     def deep(parent):
         for el in parent.find_all('a'):
             if el:
-                if (el.string != None):
-                    if (validateEmail(el.string) != None):
+                if el.string is not None:
+                    if validate_email(el.string) is not None:
                         results.append(el.string)
-                    if (validateUrl(str(el.get('href'))) != None):
-                        urlsList.append(validateUrl(el.get('href')))
+                    if validate_url(str(el.get('href'))) is not None:
+                        urls_list.append(validate_url(el.get('href')))
 
         for el in parent.find_all('p'):
             if el:
-                if (el.string != None):
+                if el.string is not None:
                     for myStr in el.string.split():
-                        if (len(myStr) > 5):
-                            if (myStr[len(myStr)-1] == '.'):
-                                if (validateEmail(myStr[:-1]) != None):
+                        if len(myStr) > 5:
+                            if myStr[len(myStr) - 1] == '.':
+                                if validate_email(myStr[:-1]) is not None:
                                     results.append(myStr[:-1])
                             else:
-                                if (validateEmail(myStr) != None):
+                                if validate_email(myStr) is not None:
                                     results.append(myStr)
                 else:
                     text = el.get_text()
-                    if (len(text) != 0):
+                    if len(text) != 0:
                         for myStr in text.split():
-                            if (len(myStr) > 5):
-                                if (validateEmail(myStr[:-1]) != None):
+                            if len(myStr) > 5:
+                                if validate_email(myStr[:-1]) is not None:
                                     results.append(myStr[:-1])
                                 else:
-                                    if (validateEmail(myStr) != None):
+                                    if validate_email(myStr) is not None:
                                         results.append(myStr)
 
     deep(soup)
-    for i in urlsList:
+    for i in urls_list:
         if not (i in visitedList) and len(visitedList) < includingLevel:
             visitedList.append(i)
             print(i, len(visitedList))
-            compose(parse,get_html,trim)(i)
+            compose(parse, get_html, trim)(i)
     for i in results:
-        ET.SubElement(res, 'email').text = str(i)
+        elTree.SubElement(res, 'email').text = str(i)
     return results
 
-tree = ET.parse(links_file_path).getroot()
+
+tree = elTree.parse(links_file_path).getroot()
 for link in tree.findall('url'):
-        res = ET.SubElement(root, 'result')
-        ET.SubElement(res, 'url').text = link.text
-        print (link.text)
-        visitedList.clear()
-        compose(parse, get_html, trim)(link.text)
-
-
+    res = elTree.SubElement(root, 'result')
+    elTree.SubElement(res, 'url').text = link.text
+    print(link.text)
+    visitedList.clear()
+    compose(parse, get_html, trim)(link.text)
 
 indent(root)
-resTree = ET.ElementTree(root)
+resTree = elTree.ElementTree(root)
 resTree.write(res_file_path)
